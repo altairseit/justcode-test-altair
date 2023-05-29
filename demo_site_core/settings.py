6134c769dafb2fc39a86eb97c93688386150d522
scaryzzzzz <dzokerseit@gmail.com>
2023-05-29 20:57:51 +0600

    changes

--- demo_site_core/settings.py
+++ demo_site_core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@0e*5%oi8e1@j=*1%vdcbs#3e5+f0$npagpj33s=fmv#__se6g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
